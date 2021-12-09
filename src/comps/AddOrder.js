2423807ff7e3974b3f736e07f0e5cea110cd8d1f
Tushar <tushar.code05@gmail.com>
2021-12-10 04:57:15 +1100

    wip

--- src/comps/AddOrder.js
+++ src/comps/AddOrder.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
+import { Form } from 'react-bootstrap'
+import axios from 'axios';
+// import Button from '@restart/ui/esm/Button';
 
 function AddOrder() {
+    const [category, setcategory] = useState([]);
+
+    useEffect(() => {
+        get();
+    }, []);
+    const clicked = (e) =>{
+        if(e === "Select Category") return;
+        console.log(e);
+        get();
+    }
+
+    const get = async () => {
+        const categories = await axios.get("https://t-m-o.herokuapp.com/category");
+        // const items = await axios.get("https://t-m-o.herokuapp.com/items");
+        if (categories.status === 200) {
+            setcategory(categories.data);
+        }
+    }
     return (
         <div>
-            <h1>reached here</h1>
+            <Form style={{width:'170px'}}>
+                <Form.Group controlId="category" onChange={()=>clicked(document.getElementById('category').value)}>
+                    <Form.Select defaultValue="Choose...">
+                        <option >Select Category</option>
+                        {!category.data ?<option value={null}>loading...</option>
+                        : category.data.map((item, index) => {
+                            return (<option key={index} value={item._id}>{item.name}</option>)
+                        })}
+                    </Form.Select>
+                </Form.Group>
+            </Form>
         </div>
     )
 }
