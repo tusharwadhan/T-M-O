d384a7d64ad6b770c57237854ba8106107c587a6
Tushar <tushar.code05@gmail.com>
2021-12-11 16:59:02 +1100

    checking image

--- src/comps/AddOrder.js
+++ src/comps/AddOrder.js
@@ -1,6 +1,7 @@
 import { React, useEffect, useState } from 'react'
 import axios from 'axios';
-import { Table } from 'react-bootstrap';
+import { Table,Image } from 'react-bootstrap';
+import logo from '../tmoLogo.png'
 
 function AddOrder() {
     const [category, setcategory] = useState([]);
@@ -22,32 +23,39 @@ function AddOrder() {
     }
     return (
         <>
-        <div>
-            {!category.data ?<h1>loading...</h1>: category.data.map((cat,cIndex)=>{
-                return(<>
-                    <br/>
-                    
-                    <Table responsive="sm">
-                    
-                        <thead></thead>
-                        <h3 key={cIndex}>{cat.name}</h3>
-                        
-                    <hr/>
-                    <tbody>
-                    <div>
-                    {item.data && item.data.map((product,pIndex)=>{
-                        return(cat._id === product.category_id &&<>
-                            <p>{product.name}</p>
-                            </>
-                        )
-                    })}
-                    </div>
-                    </tbody>
-                    </Table>
+            <div>
+                {!category.data ? <h1>loading...</h1> : category.data.map((cat, cIndex) => {
+                    return (<>
+                        <br />
+
+                        <Table responsive="sm">
+
+
+                            <thead>
+                                <h3 key={cIndex}>{cat.name}</h3>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <div>
+                                        {item.data && item.data.map((product, pIndex) => {
+                                            return (cat._id === product.category_id && <>
+                                                
+                                                    <Image src={logo} roundedCircle />
+                                                
+                                                <p>{product.name}</p>
+                                                <p>{product.name}</p>
+                                            </>
+                                            )
+                                        })}
+                                    </div>
+                                </tr>
+                            </tbody>
+                        </Table>
                     </>
-                )
-            })}
-        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
